6579b26ec6495490b675951ada88de58b88b99e4
smithm4949 <smithm4949@gmail.com>
2023-01-31 12:09:30 -0500

    adding path for api routes in server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 // const fs = require('fs');
 // const { v4: uuidv4 } = require('uuid');
-const api = require()
+const api = require('./routes/index');
 
 const PORT = process.env.PORT || 3001;
 
